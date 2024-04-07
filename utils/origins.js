ace0c7a6b40c43ad9cfae7ffcd112d6ecb15eb98
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2024-04-07 14:44:33 +0300

    some origin fixes

--- utils/origins.js
+++ utils/origins.js
@@ -1,7 +1,7 @@
 const origins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://writing-frontend-zz4t.vercel.app/',
+    'https://writing-frontend-zz4t.vercel.app',
 ];
 
 module.exports = origins;
