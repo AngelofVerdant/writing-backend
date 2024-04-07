610ee43a79d1f73c4ac8f8025395e384ff06a167
Kifanga Mukundi <pkmymcmbnhs@gmail.com>
2024-04-07 14:42:45 +0300

    added vercel to origins

--- utils/origins.js
+++ utils/origins.js
@@ -1,7 +1,7 @@
 const origins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://www.yourwebsite.com',
+    'https://writing-frontend-zz4t.vercel.app/',
 ];
 
 module.exports = origins;
